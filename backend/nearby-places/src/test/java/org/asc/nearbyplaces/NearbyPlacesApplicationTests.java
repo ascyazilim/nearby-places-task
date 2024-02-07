fcd7c64272c59bae9dfc2145292e7e16c7342601
ascyazilim <yazilimasc@gmail.com>
2024-02-07 20:49:26 +0300

    backend part created

---
+++ backend/nearby-places/src/test/java/org/asc/nearbyplaces/NearbyPlacesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.asc.nearbyplaces;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NearbyPlacesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
